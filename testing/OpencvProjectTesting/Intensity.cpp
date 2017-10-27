387ed38140c1343037f060a44979b828ac77c8e4
Nicklas <nolse16@student.aau.dk>
2017-10-28 00:43:29 +0200

    All comments done

--- testing/OpencvProjectTesting/Intensity.cpp
+++ testing/OpencvProjectTesting/Intensity.cpp
@@ -1,52 +1,58 @@
 #include "stdafx.h"
+//include header of this class
 #include "Intensity.h"
-/*
-class Intensity
-{
-	int amount = 0;
-	int final_culmProbability;
-	float probability, culmProbability;
-public:
-	void set_amount();
-	void set_probability(int);
-	int set_culmProbability(int);
-	void set_final_culmProbability();
-};
-*/
 
+//function that increases objects amount count
 void Intensity::set_amount() {
 	amount++;
 }
 
-void Intensity::set_amount(int n_amount) {
-	amount = n_amount;
-}
-
+//function that returns the count of the intensity object's amount variable
 int Intensity::get_amount() {
 	return amount;
 }
 
+//function that changes the Intensity object's variable histAmount with a specified int variable
+void Intensity::set_histAmount(int n_amount) {
+	hist_amount = n_amount;
+}
+
+//function that returns the count of the intensity object's histAmount variable
+int Intensity::get_histAmount() {
+	return hist_amount;
+}
+
+//function that calculates and sets the probability of an Intensity's value
+//by dividing the count of the intensity-value by the given image's pixel amount
+//which is passed through the function call
 void Intensity::set_probability(int pixelAmount) {
 	probability = (float)amount / pixelAmount;
 }
 
+//function that returns the probability of the intensity-value 
 float Intensity::get_probability() {
 	return probability;
 }
 
+//function that calculates and sets the culminated probability
 float Intensity::set_culmProbability(float prevCulmProbability) {
 	culmProbability = prevCulmProbability + probability;
 	set_final_culmProbability();
 	return culmProbability;
 }
 
+//function that returns the culminated probability
 float Intensity::get_culmProbability() {
 	return culmProbability;
 }
+
+//function that calculates and sets the final culminated probability or the new equalized value for
+//the intensity-value
 void Intensity::set_final_culmProbability() {
 	final_culmProbability = floor(culmProbability * 255);
 }
 
+//function that returns the equalized intensity-value
 int Intensity::get_final_culmProbability() {
 	return final_culmProbability;
 }
--- testing/OpencvProjectTesting/Intensity.h
+++ testing/OpencvProjectTesting/Intensity.h
@@ -1,15 +1,16 @@
 #pragma once
 #include "cmath"
-
+//header for the class Intensity
 class Intensity
 {
 	int amount = 0;
-	int final_culmProbability;
+	int final_culmProbability, hist_amount;
 	float probability, culmProbability;
 public:
 	void set_amount();
-	void set_amount(int n_amount);
 	int get_amount();
+	void set_histAmount(int n_amount);
+	int get_histAmount();
 	void set_probability(int);
 	float get_probability();
 	float set_culmProbability(float);
@@ -17,30 +18,3 @@ public:
 	void set_final_culmProbability();
 	int get_final_culmProbability();
 };
-
-/*
-void Intensity::set_amount() {
-	amount++;
-}
-
-int Intensity::get_amount() {
-	return amount;
-}
-
-void Intensity::set_probability(int pixelAmount) {
-	probability = (float) amount / pixelAmount;
-}
-
-float Intensity::set_culmProbability(float prevCulmProbability) {
-	culmProbability = prevCulmProbability + probability;
-	set_final_culmProbability();
-	return culmProbability;
-}
-void Intensity::set_final_culmProbability() {
-	final_culmProbability = (int) floor(culmProbability) * 255;
-}
-
-int Intensity::get_final_culmProbability() {
-	return final_culmProbability;
-}
-*/
